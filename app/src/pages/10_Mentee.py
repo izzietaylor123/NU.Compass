2930a999e801ba98fc5cdeac029b4d8b4bb7ee95
Sarah2179 <110194195+Sarah2179@users.noreply.github.com>
2024-11-25 09:09:10 -0500

    User story 1 edits 11/25

--- app/src/pages/10_Mentee.py
+++ app/src/pages/10_Mentee.py
@@ -9,22 +9,27 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome USAID Worker, {st.session_state['first_name']}.")
+st.title(f"Alumni Mentor, {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('My blurb: learn about me!', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/11_Prediction.py')
 
-if st.button('View the Simple API Demo', 
+if st.button('Take a look at my course, accomodations, and atmosphere ratings!', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/12_API_Test.py')
 
-if st.button("View Classification Demo",
+if st.button("Take a look at my mentorship replies!",
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/13_Classification.py')
+
+if st.button("Take a look at my personal mentor view",
              type='primary',
              use_container_width=True):
   st.switch_page('pages/13_Classification.py')
