c08460738b2bc60a548c793ac072e5a8e2fe9f4b
Isabel Taylor <166870184+izzietaylor123@users.noreply.github.com>
2024-12-01 20:36:33 -0500

    syntax correction

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from backend.db_connection import db
-from api.backend.location.location_routes import locations
+from api.backend.customers.location_routes import locations
 from backend.products.products_routes import products
 from backend.simple.simple_routes import simple_routes
 import os
