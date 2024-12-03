153bc1e5709b96fc087859c5d31042092c0fadf4
ashnashah28 <shah.ashn@northeastern.edu>
2024-12-03 14:38:05 -0500

    mentor/mentee mgmt routes

---
+++ api/backend/mentor_mentee_management/mentee_management_routes copy.py
@@ -0,0 +1,26 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
+abroad_programs = Blueprint('abroad_programs', __name__)
+
+
+#------------------------------------------------------------
+# Get all mentors from the system
+@abroad_programs.route('/mentors', methods=['GET'])
+def get_all_mentors():
+
+    cursor = db.get_db().cursor()
+    query = '''SELECT sID, fName, lName, email, blurb, role FROM Students
+    WHERE role = mentor'''
+    cursor.execute(query)
+    
+    mentors = cursor.fetchall()
+    
+    the_response = make_response(jsonify(mentors))
+    the_response.status_code = 200
+    return the_response
