3831969836288a027bedd470e5ffe06073c447ec
laragoyal <goyal.lara@gmail.com>
2024-12-02 21:12:00 -0500

    p4 edit

--- app/src/Home.py
+++ app/src/Home.py
@@ -83,7 +83,7 @@ if st.button('Act as Adam Brody, a Northeastern Global Experience Staff Member a
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'GESM'
     st.session_state['first_name'] = 'Adam'
-    st.switch_page('pages/20_Admin_Home.py');
+    st.switch_page('pages/26_GEOAdmin.py');
 
 
 
