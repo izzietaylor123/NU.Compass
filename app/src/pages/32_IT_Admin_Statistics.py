cdd5db3cf973322e96c6ae2be58e0693a4b75b3f
ashnashah28 <shah.ashn@northeastern.edu>
2024-12-03 20:19:25 -0500

    program by country pie chart

--- app/src/pages/32_IT_Admin_Statistics.py
+++ app/src/pages/32_IT_Admin_Statistics.py
@@ -25,20 +25,31 @@ locations = requests.get('http://api:4000/l/locations').json()
 
 # Make a bar chart comparing the number of mentors to mentees
 user_counts = {}
-user_counts["mentor"] = 0
-user_counts["mentee"] = 0
+user_counts["Mentor"] = 0
+user_counts["Mentee"] = 0
 
 for mentor in mentor_data:
-    user_counts["mentor"] += 1
-
+    user_counts["Mentor"] += 1
 for mentee in mentee_data:
-     user_counts["mentee"] += 1
+     user_counts["Mentee"] += 1
 
+st.write(f"Number of Mentees: {user_counts['Mentee']}, Number of Mentors: {user_counts['Mentor']}")
 plt.bar(user_counts.keys(), user_counts.values(), color=['blue', 'red'])
 plt.title('Mentor vs Mentee')
 plt.xlabel('Category')
 plt.ylabel('Count')
-
 st.pyplot(plt)
 
-st.write(f"{user_counts}")
+# Make a pie chart of abroad programs separated by country
+location_counts = {}
+for location in locations:
+     country = location["country"]
+     if country: 
+            location_counts[country] = location_counts.get(country, 0) + 1
+
+st.write(f"{location_counts}")
+labels = location_counts.keys()
+sizes = location_counts.values()
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels, autopct='%1.1f%%', pctdistance=.85, labeldistance=1.4)
+st.pyplot(plt)
