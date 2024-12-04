cef84e52e4017b103172a7e6c0a1b7fd78f36413
ashnashah28 <shah.ashn@northeastern.edu>
2024-12-03 19:52:27 -0500

    user plot

--- app/src/pages/32_IT_Admin_Statistics.py
+++ app/src/pages/32_IT_Admin_Statistics.py
@@ -4,6 +4,7 @@ logger = logging.getLogger(__name__)
 import streamlit as st
 from modules.nav import SideBarLinks
 import requests
+import matplotlib.pyplot as plt
 
 from st_keyup import st_keyup
 
@@ -22,3 +23,22 @@ mentee_data = requests.get('http://api:4000/s/mentees').json()
 abroad_programs_data = requests.get('http://api:4000/ap/abroad_programs').json()
 locations = requests.get('http://api:4000/l/locations').json()
 
+# Make a bar chart comparing the number of mentors to mentees
+user_counts = {}
+user_counts["mentor"] = 0
+user_counts["mentee"] = 0
+
+for mentor in mentor_data:
+    user_counts["mentor"] += 1
+
+for mentee in mentee_data:
+     user_counts["mentee"] += 1
+
+plt.bar(user_counts.keys(), user_counts.values(), color=['blue', 'red'])
+plt.title('Mentor vs Mentee')
+plt.xlabel('Category')
+plt.ylabel('Count')
+
+st.pyplot(plt)
+
+st.write(f"{user_counts}")
