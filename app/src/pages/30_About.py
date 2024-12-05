6c288346746fe833248b62a0010fd8f30876e3af
Isabel Taylor <166870184+izzietaylor123@users.noreply.github.com>
2024-12-05 14:15:23 -0500

    updated app about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,9 +8,10 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    This is a demo app for a CS 3200 Course Project by Isabel Taylor, Ashna Shah, Sarah Wang, Ava Toren and Lara Goyal.  
 
-    The goal of this demo is to mock an app that can create bridges between students on abroad programs. 
+    The goal of this demo is to mock an app that can create bridges between students on abroad programs, giving them resources to 
+    learn more about programs and connect with one another. 
 
     We hope that it can help students preparing to go abroad make informed decisions and feel more confident in the adventure that 
     awaits them, and that students returing from being abroad can give back to and maintain their abroad community!
