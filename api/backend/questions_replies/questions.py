f2f1a2ddff63184b1ed9a9fddbfd821e6fbbfdb9
Sarah2179 <110194195+Sarah2179@users.noreply.github.com>
2024-12-05 13:57:53 -0500

    added ability to post questions and replies

--- api/backend/questions_replies/questions.py
+++ api/backend/questions_replies/questions.py
@@ -80,3 +80,51 @@ def get_student_questions(sID):
     the_response = make_response(jsonify(theData))
     the_response.status_code = 200
     return the_response
+
+# #------------------------------------------------------------
+# Add new question  
+@questions_replies.route('/add_question', methods=['POST'])
+def add_question():
+
+    question_data = request.json
+
+    sID = question_data['sID']
+    content = question_data['content']
+
+    query = '''INSERT INTO Question (sID, content)
+    VALUES (%s, %s)'''
+
+    current_app.logger.info('Inserting question with ID: %s', sID)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query, (sID, content))
+    db.get_db().commit()
+
+    response = make_response("Successfully added new question")
+    response.status_code = 200
+    return 'question created!'
+
+# #------------------------------------------------------------
+# Add new reply  
+@questions_replies.route('/add_reply', methods=['POST'])
+def add_reply():
+
+    loc_data = request.json
+
+    loc_id = loc_data['locationID']
+    city = loc_data['city']
+    country = loc_data['country']
+    description = loc_data['description']
+
+    query = '''INSERT INTO Location (locationID, city, country, description)
+    VALUES (%s, %s, %s, %s)'''
+
+    current_app.logger.info('Inserting location with ID: %s', loc_id)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query, (loc_id, city, country, description))
+    db.get_db().commit()
+
+    response = make_response("Successfully added new location")
+    response.status_code = 200
+    return 'location created!'
